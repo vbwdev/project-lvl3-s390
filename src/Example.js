303c0f6a59a368c4166ca5c7e0cb9252230b8533
Sergey Nikolskiy <s.nikolskiy@space307.com>
2019-01-28 16:29:57 +0300

    Comment unnecessary code

--- src/Example.js
+++ src/Example.js
@@ -1,9 +1,9 @@
 export default class Example {
-  constructor(element) {
-    this.element = element;
-  }
-  init() {
-    // this.element.textContent = 'hello, world!';
-    // console.log('ehu!');
-  }
+  // constructor(element) {
+  //   this.element = element;
+  // }
+  // init() {
+  //   this.element.textContent = 'hello, world!';
+  //   console.log('ehu!');
+  // }
 }
