dc0c36ae58b1e05a86344b557eda9eb36909a95b
Sergey Nikolsky <vbwdev@gmail.com>
2019-01-29 00:36:57 +0300

    Add early validator code

--- src/init.js
+++ src/init.js
@@ -1,10 +1,44 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import isUrl from 'validator/lib/isURL';
+import WatchJS from 'melanke-watchjs';
+const watch = WatchJS.watch;
+
 // import Example from './Example';
 
 export default () => {
-  // const element = document.getElementById('point');
-  // const obj = new Example(element);
-  // obj.init();
+  const state = {
+    addUrlForm: {
+      isUrlValid: false,
+      canSubmit: false,
+    },
+  };
+
+  const rssUrlInput = document.querySelector('.js-rss-url-input');
+  const rssUrlSubmitButton = document.querySelector('.js-rss-url-submit-button');
+
+  rssUrlInput.addEventListener('input', function(e) {
+    const { value } = e.target;
+    const isValid = isUrl(value);
+    if (isValid) {
+      state.addUrlForm.isUrlValid = true;
+      state.canSubmit = true;
+    } else {
+      state.addUrlForm.isUrlValid = false;
+      state.canSubmit = false;
+    }
+  });
+
+  watch(state.addUrlForm, ['isUrlValid', 'canSubmit'], function() {
+    rssUrlInput.classList.toggle('is-invalid', !state.addUrlForm.isUrlValid);
+    rssUrlInput.classList.toggle('is-valid', state.addUrlForm.isUrlValid);
+    if (state.addUrlForm.canSubmit) {
+      rssUrlSubmitButton.disabled = false;
+    } else {
+      rssUrlSubmitButton.disabled = true;
+    }
+    console.log(rssUrlSubmitButton.disabled);
+  });
+
 };
