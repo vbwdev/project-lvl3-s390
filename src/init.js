db740d0c8806fbc157694a4822c50db88f13f646
Sergey Nikolsky <vbwdev@gmail.com>
2019-01-30 23:46:20 +0300

    Add early rss channels loading

--- src/init.js
+++ src/init.js
@@ -1,5 +1,6 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
 import isUrl from 'validator/lib/isURL';
 import WatchJS from 'melanke-watchjs';
@@ -15,13 +16,15 @@ export default () => {
       canSubscribe: false,
     },
     rssUrls: [],
+    channels: [],
+    articles: [],
   };
 
   const rssUrlForm = document.querySelector('.js-rss-url-form');
   const rssUrlInput = document.querySelector('.js-rss-url-input');
   const rssUrlSubmitButton = document.querySelector('.js-rss-url-submit-button');
 
-  rssUrlInput.addEventListener('input', (e) => {
+  rssUrlInput.addEventListener('input', e => {
     const { value: url } = e.target;
     if (url === '') {
       state.addUrlForm.hasUrlError = false;
@@ -35,7 +38,7 @@ export default () => {
     }
   });
 
-  rssUrlForm.addEventListener('submit', (e) => {
+  rssUrlForm.addEventListener('submit', e => {
     e.preventDefault();
 
     if (!state.addUrlForm.canSubscribe) {
@@ -43,23 +46,56 @@ export default () => {
     }
 
     state.rssUrls = [...state.rssUrls, rssUrlInput.value];
-    console.log('form was submited');
-  });
+    rssUrlInput.value = ''; // TODO Rework with state?
+    rssUrlSubmitButton.disabled = true; // TODO Rework with state?
 
-  rssUrlSubmitButton.addEventListener('click', (e) => {
-    console.log('rssUrlSubmitButton was clicked');
-  });
+    const rssUrls = state.rssUrls.map(rssUrl => {
+      return axios.get(`https://cors-anywhere.herokuapp.com/${rssUrl}`);
+    });
 
+    Promise.all(rssUrls)
+      .then(channelsRowData => {
+        return channelsRowData.map(({ data: channelRowData }) =>
+          new DOMParser().parseFromString(channelRowData, 'application/xml'),
+        );
+      })
+      .then(channels => {
+        const channelsData = [];
+        const itemsData = [];
+        channels.forEach(channel => {
+          const channelTitle = channel.querySelector('title').textContent;
+          const channelDescription = channel.querySelector('description').textContent;
+          channelsData.push({
+            title: channelTitle,
+            description: channelDescription,
+          });
+
+          const items = channel.querySelectorAll('item');
+          items.forEach(item => {
+            const itemTitle = item.querySelector('title').textContent;
+            const itemLink = item.querySelector('link').textContent;
+            itemsData.push({
+              title: itemTitle,
+              link: itemLink,
+            });
+          });
+        });
+        state.channels = channelsData;
+        state.articles = itemsData;
+      });
+
+  });
 
   watch(state.addUrlForm, ['hasUrlError', 'canSubscribe'], () => {
     rssUrlInput.classList.toggle('is-invalid', state.addUrlForm.hasUrlError);
     rssUrlSubmitButton.disabled = !state.addUrlForm.canSubscribe;
   });
 
-  watch(state, 'rssUrls', () => {
-    rssUrlInput.value = '';
-    rssUrlSubmitButton.disabled = true;
-    console.log('state.rssUrls was updated');
-    console.log(state.rssUrls.length, state.rssUrls);
+  watch(state, 'channels', () => {
+    console.log('channels was updated', state.channels);
+  });
+
+  watch(state, 'articles', () => {
+    console.log('articles was updated', state.articles);
   });
 };
