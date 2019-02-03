d0e32acf5c3d865fef77bc8dff3969f7d1e15808
Sergey Nikolsky <vbwdev@gmail.com>
2019-02-03 03:54:02 +0300

    Remove unused code

--- src/components/channelsList.js
+++ src/components/channelsList.js
@@ -1,12 +1,4 @@
-import getSpinnerHtml from './spinner';
-
-const getChannelsListHtml = (channels, isLoading) => {
-  if (isLoading) {
-    return getSpinnerHtml();
-  }
-  if (channels.length === 0) {
-    return '<li class="list-group-item">No channels added</li>';
-  }
+const getChannelsListHtml = channels => {
   const channelsHtml = channels.map(
     ({ title, description }) => `<li class="list-group-item"><h4>${title}</h4>${description}</li>`,
   );
--- src/components/spinner.js
+++
@@ -1,9 +0,0 @@
-const getSpinnerHtml = () => `
-  <div class="d-flex justify-content-center">
-    <div class="spinner-border text-primary">
-      <span class="sr-only">Loading...</span>
-    </div>
-  </div>
-`;
-
-export default getSpinnerHtml;
